e2b97a107d81d79d36164b5799df0f0d0b869308
Ivan Kalinin <vania_kalinin@mail.ru>
2024-11-03 18:52:34 +0300

    Reorganized the folder structure and their contents

---
+++ Implementation/Implementation/Structural_Patterns/Proxy/Classes/Prop.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementation.Behavioral_Patterns.Chain_of_Responsibility.Classes
+{
+    internal class Prop
+    {
+    }
+}
