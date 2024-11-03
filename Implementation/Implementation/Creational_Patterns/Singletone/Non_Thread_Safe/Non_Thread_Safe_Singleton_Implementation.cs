fd9b81eb2ae17f988245f3b9fe811ab95d1f44d9
Ivan Kalinin <vania_kalinin@mail.ru>
2024-11-03 21:50:13 +0300

    Implemented Singleton pattern

---
+++ Implementation/Implementation/Creational_Patterns/Singletone/Non_Thread_Safe/Non_Thread_Safe_Singleton_Implementation.cs
@@ -0,0 +1,28 @@
+﻿using Implementation.Creational_Patterns.Singletone.Non_Thread_Safe.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementation.Creational_Patterns.Singletone.Non_Thread_Safe
+{
+    internal class Non_Thread_Safe_Singleton_Implementation
+    {
+        static void Main(string[] args)
+        {
+            // Клиентский код.
+            Non_Thread_Safe_Singleton s1 = Non_Thread_Safe_Singleton.GetInstance();
+            Non_Thread_Safe_Singleton s2 = Non_Thread_Safe_Singleton.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton works, both variables contain the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed, variables contain different instances.");
+            }
+        }
+    }
+}
---
+++ Implementation/Implementation/Creational_Patterns/Singletone/Thread_Safe/Thread_Safe_Singleton_Implementation.cs
@@ -0,0 +1,45 @@
+﻿using Implementation.Creational_Patterns.Singletone.Thread_Safe.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementation.Creational_Patterns.Singletone.Thread_Safe
+{
+    internal class Thread_Safe_Singleton_Implementation
+    {
+        static void Main(string[] args)
+        {
+            // Клиентский код.
+
+            Console.WriteLine(
+                "{0}\n{1}\n\n{2}\n",
+                "If you see the same value, then singleton was reused (yay!)",
+                "If you see different values, then 2 singletons were created (booo!!)",
+                "RESULT:"
+            );
+
+            Thread process1 = new Thread(() =>
+            {
+                TestSingleton("FOO");
+            });
+            Thread process2 = new Thread(() =>
+            {
+                TestSingleton("BAR");
+            });
+
+            process1.Start();
+            process2.Start();
+
+            process1.Join();
+            process2.Join();
+        }
+
+        public static void TestSingleton(string value)
+        {
+            Thread_Safe_Singleton singleton = Thread_Safe_Singleton.GetInstance(value);
+            Console.WriteLine(singleton.Value);
+        }
+    }
+}
