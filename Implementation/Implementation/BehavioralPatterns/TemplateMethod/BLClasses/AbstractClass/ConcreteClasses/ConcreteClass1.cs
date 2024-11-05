8cd8a353a416ac51c511ac12c95e03785a534cef
Ivan Kalinin <vania_kalinin@mail.ru>
2024-11-05 19:13:16 +0300

    Implemented Template Method pattern

---
+++ Implementation/Implementation/BehavioralPatterns/TemplateMethod/BLClasses/AbstractClass/ConcreteClasses/ConcreteClass1.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Implementation.BehavioralPatterns.TemplateMethod.BLClasses.AbstractClass.ConcreteClasses
+{
+    // Конкретные классы должны реализовать все абстрактные операции базового
+    // класса. Они также могут переопределить некоторые операции с реализацией
+    // по умолчанию.
+    internal class ConcreteClass1 : AbstractClassExample
+    {
+        protected override void RequiredOperations1()
+        {
+            Console.WriteLine("ConcreteClass1 says: Implemented Operation1");
+        }
+
+        protected override void RequiredOperation2()
+        {
+            Console.WriteLine("ConcreteClass1 says: Implemented Operation2");
+        }
+    }
+}
---
+++ Implementation/Implementation/BehavioralPatterns/TemplateMethod/BLClasses/AbstractClass/ConcreteClasses/ConcreteClass2.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Implementation.BehavioralPatterns.TemplateMethod.BLClasses.AbstractClass.ConcreteClasses
+{
+    // Обычно конкретные классы переопределяют только часть операций базового
+    // класса.
+    internal class ConcreteClass2 : AbstractClassExample
+    {
+        protected override void RequiredOperations1()
+        {
+            Console.WriteLine("ConcreteClass2 says: Implemented Operation1");
+        }
+
+        protected override void RequiredOperation2()
+        {
+            Console.WriteLine("ConcreteClass2 says: Implemented Operation2");
+        }
+
+        protected override void Hook1()
+        {
+            Console.WriteLine("ConcreteClass2 says: Overridden Hook1");
+        }
+    }
+}
